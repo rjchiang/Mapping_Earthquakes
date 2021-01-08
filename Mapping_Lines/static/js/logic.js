f5e3d69a71e0f8053702aedd77d3a85d8b96bdd9
rjchiang <rayjay.chiang@pepperdine.edu>
2021-01-08 00:32:03 -0800

    background

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -44,14 +44,14 @@ L.polyline(line, {
 //    });
 
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location, {
+//     radius: city.population/100000
+//   })
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+// .addTo(map);
+// });
 
 //Skill Drill Orange circle popup
 
