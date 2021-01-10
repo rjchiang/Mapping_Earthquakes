c8c78a8e8320b49e6a45ef7ba10e1b5c471638f1
rjchiang <rayjay.chiang@pepperdine.edu>
2021-01-10 13:48:20 -0800

    Add Earthquake Data to a Map

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,160 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center and zoom level.
+//let map = L.map('mapid').setView([30, 30], 2);
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// Skill Drill SF Airport
+
+// // Coordinates for each point to be used in the polyline.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
+
+// // Create a polyline using the line coordinates and make the line black.
+// L.polyline(line, {
+//   color: "yellow"
+// }).addTo(map);
+
+// Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+//  Add a circle to the map for Los Angeles, California.
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+//  }).addTo(map);
+
+// Get data from cities.js
+//let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//    });
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location, {
+//     radius: city.population/100000
+//   })
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+// .addTo(map);
+// });
+
+//Skill Drill Orange circle popup
+
+//Skill Drill: Using the Leaflet documentation, create a light-yellow 
+//circle with black lines indicating a 300-meter radius of Central
+//Los Angeles on a dark map. (Done)
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Then we add our 'graymap' tile layer to the map.
+//streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+//let airportData = "https://raw.githubusercontent.com/rjchiang/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+
+// Accessing the Toronto airline routes GeoJSON URL.
+//let torontoData = "https://raw.githubusercontent.com/rjchiang/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/rjchiang/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+
+// Create a style for the lines.
+// let myStyle = {
+//   color: "#ffffa1",
+//   weight: 2
+// }
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data).addTo(map);
+// });
+//   {
+//   style: myStyle,
+//   onEachFeature: function(feature, layer) {
+//     layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: "
+//     + feature.properties.dst + "</h3>");
+//   }
+// })
+
+
+//Skill Drill SFO to JFK
+
+//Skill Drill popup SFA
+
+//Skill Drill add popup marker airports
+
+//Skill Drill both Street and Dark
+
+//Skill Drill routes light yellow
+
+//Skill Drill blue lines
